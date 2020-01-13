e41c829d802f5e5e01571fe57f144708b98994d4
Leslee <leelovesc@gmail.com>
2020-01-13 19:40:55 +0800

    移除原jieba—_copy项目，更改为本项目的子项目

---
+++ jieba/finalseg/prob_start.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Leslee
+# @Email    : leelovesc@gmail.com
+# @Time    : 2019.10.25 18:03
+
+P={'B': -0.26268660809250016,
+ 'E': -3.14e+100,
+ 'M': -3.14e+100,
+ 'S': -1.4652633398537678}
